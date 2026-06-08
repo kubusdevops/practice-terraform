a21ef6765888ece61951d2be3dacd32885692efa
George Akwari <devops4george@gmail.com>
2026-06-07 20:23:55 -0500

    i am enjoying terraform and github

--- mainn.tf
+++ mainn.tf
@@ -1,11 +1,11 @@
 resource "aws_instance" "george_app" {
-    ami =  "ami-00e801948462f718a"
-    instance_type = "t3.micro"
-    key_name = "kubuskey"
-    security_groups = [aws_security_group.george_sg.name]
+  ami             = "ami-00e801948462f718a"
+  instance_type   = "t3.micro"
+  key_name        = "kubuskey"
+  security_groups = [aws_security_group.george_sg.name]
 
-    tags = {
-        Name = "george-app"
-    }
-  
+  tags = {
+    Name = "george-app"
+  }
+
 }
