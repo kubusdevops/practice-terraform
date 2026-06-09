9fa148c721f0d1bda0b959817df46e2a1d972a33
George Akwari <devops4george@gmail.com>
2026-06-09 08:35:51 -0500

    second time creating EC2 via terrafor

---
+++ securitygroup.tf
@@ -0,0 +1,34 @@
+resource "aws_security_group" "georgeapp-sg" {
+    name = "georgeapp-sg"
+    description = "Security group for georgeapp"
+    vpc_id = "vpc-0f8ffa69427e755ff"
+
+}
+
+resource "aws_security_group_rule" "allow_ssh" {
+    type = "ingress"
+    from_port = 22
+    to_port = 22
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+    security_group_id = aws_security_group.georgeapp-sg.id
+}
+
+resource "aws_security_group_rule" "allow_http" {
+    type = "ingress"
+    from_port = 80
+    to_port = 80
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+    security_group_id = aws_security_group.georgeapp-sg.id
+}
+
+resource "aws_security_group_rule" "allow_outbound" {
+    type = "egress"
+    from_port = 0
+    to_port = 0
+    protocol = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+    security_group_id = aws_security_group.georgeapp-sg.id
+}
+
