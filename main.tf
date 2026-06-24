427549c103e51015d0f40f49358f3326b1e40a44
George Akwari <devops4george@gmail.com>
2026-06-24 18:13:33 -0500

    updated kubuskey.pem

--- main.tf
+++ main.tf
@@ -1,13 +1,18 @@
 resource "aws_instance" "georgeapp" {
-    ami           = "ami-0521cb2d60cfbb1a6"
-    instance_type = "t2.micro"
-    key_name      = "kubuskey"
-    security_groups = [aws_security_group.georgeapp.name]
+  ami             = "ami-0521cb2d60cfbb1a6"
+  instance_type   = "t3.micro"
+  key_name        = aws_key_pair.deployer.key_name
+  security_groups = [aws_security_group.georgeapp.name]
 
+  tags = {
+    Name = "george-app"
+  }
+  
+  user_data = file("userdata.sh")
+}
 
-    tags = {
-        Name = "george-app"
-    }
-    user_data = file("userdata.sh")
-
+resource "aws_key_pair" "deployer" {
+  # Change only this line below to fix the duplicate error:
+  key_name   = "kubuskey-new"
+  public_key = file("${path.module}/kubuskey.pub")
 }
--- provider.tf
+++ provider.tf
@@ -4,8 +4,14 @@ terraform {
     aws = {
       source  = "hashicorp/aws"
       version = "~> 5.0"
-    }
+    
   }
+}
+backend "s3" {
+  bucket = "kubus-terraform-state"
+  key    = "georgeapp/terraform.tfstate"
+  region = "us-east-1"
+}
 }
 
 # 2. Set your preferred AWS region
