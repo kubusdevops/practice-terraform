ae6b4a7ce69831ab226587b6efac8105af7a64ef
George Akwari <devops4george@gmail.com>
2026-06-24 09:07:12 -0500

    manged state file in S3 bucket

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 resource "aws_instance" "georgeapp" {
     ami           = "ami-0521cb2d60cfbb1a6"
-    instance_type = "t3.micro"
+    instance_type = "t2.micro"
     key_name      = "kubuskey"
     security_groups = [aws_security_group.georgeapp.name]
 
